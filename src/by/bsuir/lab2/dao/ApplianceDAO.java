adcb456420e8748dc9d617e13b3e2b9147f46685
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-11-07 12:29:49 +0300

    Created method for search the cheapest and the most expensive appliance.

--- src/by/bsuir/lab2/dao/ApplianceDAO.java
+++ src/by/bsuir/lab2/dao/ApplianceDAO.java
@@ -1,11 +1,11 @@
 package by.bsuir.lab2.dao;
 
 import by.bsuir.lab2.entity.Appliance;
-import by.bsuir.lab2.entity.criteria.Criteria;
 
 import java.util.ArrayList;
 
 public interface ApplianceDAO {
-    ArrayList<Appliance> find(Criteria var1);
     ArrayList<Appliance> findByApplianceType(Class applianceType);
+    ArrayList<Appliance> findTheCheapestAppliance();
+    ArrayList<Appliance> findTheMostExpensiveAppliance();
 }
--- src/by/bsuir/lab2/dao/ApplianceDAOImpl.java
+++ src/by/bsuir/lab2/dao/ApplianceDAOImpl.java
@@ -1,13 +1,13 @@
 package by.bsuir.lab2.dao;
 
 import by.bsuir.lab2.entity.Appliance;
-import by.bsuir.lab2.entity.criteria.Criteria;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.beans.Expression;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -19,10 +19,6 @@ public class ApplianceDAOImpl implements ApplianceDAO {
 
     public ApplianceDAOImpl() {
         xmlDocument = getDocument();
-    }
-
-    public ArrayList<Appliance> find(Criteria criteria) {
-        return null;
     }
 
     private Document getDocument() {
@@ -48,20 +44,75 @@ public class ApplianceDAOImpl implements ApplianceDAO {
                 Object currAppliance = null;
 
                 try {
-                    currAppliance = applianceType.getConstructor(Node.class).newInstance(new Object[] { appliance });
-                } catch (InstantiationException e) {
-                    throw new RuntimeException(e);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                } catch (InvocationTargetException e) {
-                    throw new RuntimeException(e);
-                } catch (NoSuchMethodException e) {
+                    currAppliance = applianceType.getConstructor(Node.class).newInstance(appliance);
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                     throw new RuntimeException(e);
                 }
 
                 findAppliances.add((Appliance) currAppliance);
             }
 
+        }
+        return findAppliances;
+    }
+
+    interface Comparison{
+        boolean comp(double a, double b);
+    }
+    public ArrayList<Appliance> findTheCheapestAppliance() {
+        return findMaxOrMinPrice((x, y) -> x < y);
+    }
+
+    public ArrayList<Appliance> findTheMostExpensiveAppliance() {
+        return findMaxOrMinPrice((x, y) -> x > y);
+    }
+
+    private ArrayList<Appliance> findMaxOrMinPrice(Comparison comparison) {
+        ArrayList<Appliance> findAppliances = new ArrayList<>();
+        NodeList prices = xmlDocument.getDocumentElement().getElementsByTagName("price");
+        Node appliance;
+
+        double minPrice = Double.parseDouble(prices.item(0).getTextContent());
+        double currPrice;
+        for (int i = 1; i < prices.getLength(); i++) {
+            try {
+                currPrice = Double.parseDouble(prices.item(i).getTextContent());
+                if (comparison.comp(currPrice, minPrice)) {
+                    minPrice = currPrice;
+                }
+            } catch (Exception e) {
+            }
+        }
+
+        for (int i = 0; i < prices.getLength(); i++) {
+            try {
+                currPrice = Double.parseDouble(prices.item(i).getTextContent());
+            } catch (Exception e) {
+                continue;
+            }
+
+            if (minPrice == currPrice) {
+                appliance = prices.item(i).getParentNode();
+                NodeList nodes = appliance.getChildNodes();
+
+                int j = 0;
+                String field;
+                do {
+                    field = nodes.item(j).getNodeName();
+                    j++;
+                } while (j < nodes.getLength() && !field.equals("type"));
+                String className = "by.bsuir.lab2.entity." + nodes.item(j - 1).getTextContent();
+
+                Object currAppliance = null;
+                try {
+                    currAppliance = Class.forName(className).getConstructor(Node.class).newInstance(appliance);
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+                findAppliances.add((Appliance) currAppliance);
+            }
         }
         return findAppliances;
     }
